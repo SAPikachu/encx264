e315c06647ca9e11256d10defbe23a7ae27ab504
SAPikachu <sapikachu@gmail.com>
2011-03-14 21:03:32 +0800

    build script

---
+++ build.cmd
@@ -0,0 +1,20 @@
+rd /s /q build 2>nul
+call d:\Python31\Scripts\cxfreeze.bat encx264.py --target-dir build
+md build\pyd
+move build\*.pyd build\pyd
+rd /s /q tmp
+md tmp
+copy encx264.py tmp
+cd tmp
+echo. > imports.py
+for %%f in (..\impls\*.py) do copy /b imports.py + "%%f" imports.py
+call d:\Python31\Scripts\cxfreeze.bat encx264.py --target-dir build --include-modules imports
+copy /y build\encx264.exe ..\build
+cd ..
+rd /s /q tmp
+md build\impls
+copy impls build\impls
+copy encx264_targets.py.sample build
+del build\impls\*.pyc
+rd /s /q build\impls\__pycache__
+pause
--- encx264.py
+++ encx264.py
@@ -1,4 +1,8 @@
 import sys
+import os
+
+# support build script
+sys.path.append(os.path.join(os.path.dirname(sys.path[0]), "pyd"))
 
 if len(sys.argv) > 1 and sys.argv[1][0] == "!":
     from impls.functions import functions

dbb9e81daa49bb9263a3d8ff70fe322c6fb18ac3
SAPikachu <sapikachu@gmail.com>
2011-03-15 19:59:10 +0800

    fix empty sys.argv[0] in loader

--- encx264.py
+++ encx264.py
@@ -3,7 +3,13 @@ import os
 
 # support build script
 sys.path.append(os.path.join(os.path.dirname(sys.path[0]), "pyd"))
-
+if not sys.argv[0]:
+    entry_point = sys.path[0]
+    if (not entry_point.lower().endswith(".py")) and \
+       (not entry_point.lower().endswith(".exe")):
+        entry_point = os.path.join(entry_point, "encx264.py")
+    sys.argv[0] = entry_point
+    
 if len(sys.argv) > 1 and sys.argv[1][0] == "!":
     from impls.functions import functions
     name = sys.argv[1][1:]

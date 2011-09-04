29d6a7302d836903899c385728b45299e0aba306
SAPikachu <sapikachu@gmail.com>
2011-09-04 12:51:43 +0800

    add a command to print version

--- impls/functions.py
+++ impls/functions.py
@@ -1,5 +1,6 @@
 from .update import update
 from .task import task_do_command
+from .version import print_version
 import os
 
 __all__  = ["function"]
@@ -9,4 +10,5 @@ base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 functions = {
     "update": lambda: update(base_path=base_dir),
     "task": task_do_command,
+    "version": print_version,
 }
